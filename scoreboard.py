fed7277ae5b759b603dc589017813fefe9e913ae
Scott Stevens <go.scott.mac@gmail.com>
2018-08-20 23:28:27 +0930

    WIP maps editor: Improved + added callbacks, outputs.

--- scoreboard.py
+++ scoreboard.py
@@ -1,6 +1,9 @@
+import os
 import re
 import shutil
 import unicodedata
+
+from contextlib import suppress
 
 import kivy
 import kivy.app
@@ -11,7 +14,7 @@ from kivy.uix.tabbedpanel import TabbedPanel
 # from kivy.core.window import Window
 from kivy.uix.boxlayout import BoxLayout
 # from kivy.properties import ObjectProperty
-from kivy.logger import Logger
+# from kivy.logger import Logger
 from kivy.clock import Clock
 
 # Builder.load_file("scoreboard.kv")
@@ -27,15 +30,20 @@ def filename_fmt(val):
 
 
 def text_fmt(val):
-    return val.upper()
+    return str(val).upper()
 
 
-def copyfile(src, dest):
+def copyfile(src, dest, delete_if_missing=True):
     # Prevent crashes by outputting a fallback if we can't get the file.
     try:
         shutil.copyfile(src, dest)
     except FileNotFoundError:
-        shutil.copyfile(IMAGEROOT + "/fallback.png", dest)
+        # Either delete the image or default to a "missing" image.
+        if delete_if_missing:
+            with suppress(FileNotFoundError):
+                os.remove(dest)
+        else:
+            shutil.copyfile(IMAGEROOT + "/missing.png", dest)
 
 
 # TODO some modal confirmation... or none at all?
@@ -43,23 +51,31 @@ def copyfile(src, dest):
 
 
 class MapManager(BoxLayout):
-    def __init__(self, *args, mapset=[], current=None, **kwargs):
-        Logger.info("Mapwidget: " + str(args) + str(kwargs))
+    def __init__(self, *args, attackers="", mapstyle="Strips",
+                 mapset=[], current=None, **kwargs):
+        # Logger.info("Mapwidget: " + str(args) + str(kwargs))
         super().__init__(*args, **kwargs)
         # IDs from kv-lang: mapset
 
         def finish(dt):
             # Fix: Wait for instantiation to finish before accessing ids.
-            for i in mapset:
-                self.addmap(i)
+            self.attackers.text = attackers
+            self.mapstyle.text = mapstyle
+            for child in mapset:
+                self.addmap(child)
             # children is a stack, so current=0 is actually the last item.
             # The export function sets this accordingly.
             _current = current
             if _current is not None:
                 _current = self.mapset.children[current]
             self.setcurrentmap(current)
+            self.draw()
 
         Clock.schedule_once(finish)  # https://stackoverflow.com/a/26918422/
+
+    @property
+    def style(self):
+        return self.mapstyle.text
 
     def addmap(self, instance=None):
         # Add a map, either by dict args or a MapWidget already instantiated.
@@ -68,20 +84,57 @@ class MapManager(BoxLayout):
         elif isinstance(instance, dict):
             instance = MapWidget(**instance)  # Import values.
         self.mapset.add_widget(instance)
-        self.autocurrentmap()
+
+        # Anything done here will be overridden by the final() sub-function in
+        # the MapWidget constructor. Instead, place code in that sub-function.
 
     def setcurrentmap(self, map):
         self.current = map
-        for i in self.mapset.children:
-            i.iscurrent = i is map
+        for child in self.mapset.children:
+            child.iscurrent = child is map
+        self.draw_livescore()
 
     def autocurrentmap(self):
         # The first non-final map will be the current one.
-        for i in reversed(self.mapset.children):
-            if not i.isfinal:
-                self.setcurrentmap(i)
+        for child in reversed(self.mapset.children):
+            if not child.isfinal:
+                self.setcurrentmap(child)
                 return
         self.setcurrentmap(None)
+
+    def callback_mapstyle(self):
+        for child in self.mapset.children:
+            child.draw_map()
+
+    def draw_totalscore(self):
+        teams = [0] * 3  # Each index represents that team (0 is draws)
+        for child in self.mapset.children:
+            teams[child.winner] += 1
+        for team, wins in enumerate(teams):
+            if not team:
+                continue  # Skip team "0" (draws).
+            target = "{}/livetotal{}.txt".format(OUTPUTROOT, team)
+            with open(target, 'w') as f:
+                f.write(text_fmt(wins))
+
+    def draw_livescore(self, team=None):
+        if team is None:
+            # Draw live scores for teams.
+            for team in (1, 2):
+                self.draw_livescore(team)
+            return
+        target = "{}/livescore{}.txt".format(OUTPUTROOT, team)
+        with open(target, 'w') as f:
+            score = 0  # Default if no live map.
+            if self.current is not None:
+                score = getattr(self.current, "score{}".format(team)).text
+            f.write(text_fmt(score))
+
+    def draw(self):
+        for child in self.mapset.children:
+            child.draw()  # Full redraw of each child.
+        self.draw_totalscore()
+        self.draw_livescore()  # team=None causes all to redraw.
 
     def __export__(self):
         # mapset.children is a stack, so we have to reverse for export.
@@ -90,14 +143,24 @@ class MapManager(BoxLayout):
         if current is not None:
             current = self.mapset.children.index(current)
         return {
+            'attackers': self.attackers.text,
+            'mapstyle': self.style,
             'current': current,
-            'mapset': [m.__export__() for m in reversed(self.mapset.children)],
+            'mapset': [i.__export__() for i in reversed(self.mapset.children)],
+
         }
 
 
 class MapWidget(BoxLayout):
-    def __init__(self, *args,
-                 pool="", map="", score1="", score2="", final=False, **kwargs):
+    def __init__(self, *args, pool="", map="",
+                 score1="0", score2="0", final=False, **kwargs):
+
+        # WARNING
+        # It is expected that a MapWidget will be immediately added to a
+        # MapManager (before the caller returns). If this is not the case, then
+        # the widget won't be rendered (when the Clock event fires), and this
+        # code will throw a myriad of exceptions.
+
         # Logger.info("Mapwidget: " + str(args) + str(kwargs))
         super().__init__(*args, **kwargs)
         self.suppress_callback_current = False
@@ -105,34 +168,24 @@ class MapWidget(BoxLayout):
 
         def finish(dt):
             # Fix: Wait for instantiation to finish before accessing ids.
-            self.iscurrent = False  # May be set by MapManager later.
+            self.iscurrent = False  # May be set later.
             self.pool.text = pool  # Triggers map selector update.
             self.pool.values = MAPS.keys()
             self.map.text = map
-            self.score1.value = score1
-            self.score2.value = score2
+            self.score1.text = score1
+            self.score2.text = score2
             self.final.active = final  # Do we need callback protection here?
+
+            # The following code ought to be in the MapManager.addmap method,
+            # but cannot be as this finish() sub-function overrides set values.
+            self.draw()
+            self.manager.autocurrentmap()
 
         Clock.schedule_once(finish)  # https://stackoverflow.com/a/26918422/
 
     @property
     def iscurrent(self):
         return self.current.active
-
-    @property
-    def isfinal(self):
-        return self.final.active
-
-    @property
-    def mapstyle(self):
-        return self.mapstyle.text
-
-    @property
-    def index1(self):
-        # Return the 1-indexed position of this element in the parent.
-        # Good for filenames, especially if being used by non-programmers.
-        siblings = self.parent.children
-        return len(siblings) - siblings.index(self)
 
     @iscurrent.setter
     def iscurrent(self, val):
@@ -141,13 +194,51 @@ class MapWidget(BoxLayout):
             self.suppress_callback_current = True
             self.current.active = val
 
+    @property
+    def isfinal(self):
+        return self.final.active
+
+    @property
+    def index1(self):
+        # Return the 1-indexed position of this element in the parent.
+        # Good for filenames, especially if being used by non-programmers.
+        siblings = self.parent.children
+        return len(siblings) - siblings.index(self)
+
+    @property
+    def winner(self):
+        # Returns number of the winning team (1-indexed). 0 is draw/incomplete.
+        if not self.isfinal:
+            return 0
+        team1 = 0  # Defaults.
+        team2 = 0
+        with suppress(ValueError):
+            # In case the user enters non-int.
+            team1 = int(self.score1.text)
+            team2 = int(self.score2.text)
+
+        if team1 > team2:
+            return 1
+        elif team2 > team1:
+            return 2
+        else:
+            return 0
+
+    @property
+    def manager(self):
+        return self.parent.root
+
+    # The following are all event-driven methods. Draw methods always output
+    # output; callback functions may call draw methods, but do not directly
+    # output. Both callback and draw functions may have KVlang triggers.
+
     def callback_current(self, on):
         if self.suppress_callback_current:
             # Updating the value also
             self.suppress_callback_current = False
             return
 
-        self.parent.root.setcurrentmap(self if on else None)
+        self.manager.setcurrentmap(self if on else None)
         if on and self.final.active:
             self.final.active = False
 
@@ -156,50 +247,82 @@ class MapWidget(BoxLayout):
         # (They cannot be both on, and both off might set current on.)
         # NEW: If we turn it on then redraw too (in case no current map).
 
-        fn = "{}/map{}result.txt".format(OUTPUTROOT, self.index1)
-        with open(fn, 'w') as f:
-            # The file is now blank regardless, so if not final, write nothing.
-            if on:
-                f.write("Winner")
+        self.draw_result()
 
+        # TODO callback suppression for the init.
         # No need to suppress this callback; it will only turn off final when
         # current is on, so the if condition is already False.
         if on or not self.current.active:
-            self.parent.root.autocurrentmap()
+            self.manager.autocurrentmap()
 
     def callback_delete(self):
         # TODO Some modal here.
-        parent = self.parent  # Keep reference after deletion.
-        parent.remove_widget(self)
-        parent.root.autocurrentmap()
+        # TODO cleanup.
+        manager = self.manager  # Keep reference after deletion.
+        self.parent.remove_widget(self)
+        manager.autocurrentmap()
 
     def callback_pool(self, pool):
         self.map.values = MAPS[pool]
         self.map.text = ""
-        fn = "{}/game/modes/{}.png".format(IMAGEROOT, filename_fmt(pool))
-        out = "{}/map{}pool.png".format(OUTPUTROOT, self.index1)
-        copyfile(fn, out)  # Will replace.
+        self.draw_pool()
 
-    def callback_map(self, map):
-        # Now set the map image to the correct image.
-        style = filename_fmt(self.parent.root.mapstyle.text)
+    def draw_pool(self):
+        pool = filename_fmt(self.pool.text)
+        infile = "{}/game/modes/{}.png".format(IMAGEROOT, pool)
+        target = "{}/map{}pool.png".format(OUTPUTROOT, self.index1)
+        copyfile(infile, target)
+
+    def draw_map(self):
+        # Set the map image to the correct image.
+        style = filename_fmt(self.manager.style)
         if self.isfinal:
             style += " desat"
+
         # Generate map name, or use the map pool, underscores must be allowed.
-        map = filename_fmt(map) or ("_pool " + filename_fmt(self.pool.text))
-        fn = "{}/maps/{}/{}.png".format(IMAGEROOT, style, map)
-        out = "{}/map{}.png".format(OUTPUTROOT, self.index1)
-        copyfile(fn, out)  # Will replace.
+        map = filename_fmt(self.map.text)  # Map (image) name.
+        if not map:
+            # If no map, use the pool image instead.
+            # No map and no pool? No image (copyfile places "missing.png").
+            map = "_pool " + filename_fmt(self.pool.text)
 
-    def callback_score1(self, value):
-        fn = "{}/map{}score1.txt".format(OUTPUTROOT, self.index1)
-        with open(fn, 'w') as f:
-            f.write(value)
+        infile = "{}/maps/{}/{}.png".format(IMAGEROOT, style, map)
+        target = "{}/map{}.png".format(OUTPUTROOT, self.index1)
+        copyfile(infile, target, delete_if_missing=False)
 
-    def callback_score2(self, value):
-        fn = "{}/map{}score2.txt".format(OUTPUTROOT, self.index1)
-        with open(fn, 'w') as f:
-            f.write(value)
+    def draw_score(self, team=None):
+        if team is None:
+            for team in (1, 2):
+                self.draw_score(team)  # Draw for all teams.
+
+        # Team is 1 or 2, depending on which score value was changed.
+        target = "{}/map{}score{}.txt".format(OUTPUTROOT, self.index1, team)
+        with open(target, 'w') as f:
+            text = getattr(self, "score{}".format(team))  # Input field.
+            f.write(text_fmt(text.text))
+        if self.iscurrent:
+            self.manager.draw_livescore(team)
+        if self.isfinal:
+            self.draw_result()
+
+    def draw_result(self):
+        target = "{}/map{}result.txt".format(OUTPUTROOT, self.index1)
+        with open(target, 'w') as f:
+            # The file is now blank regardless, so if not final, write nothing.
+            if self.isfinal:
+                win = self.winner  # 0 draw (not incomplete), else team number.
+                if win != 0:
+                    f.write(text_fmt("Team {}".format(win)))
+                else:
+                    f.write(text_fmt("Draw!"))
+        self.manager.draw_totalscore()  # Changing the result changes totals.
+
+    def draw(self):
+        # Redraws all.
+        self.draw_pool()
+        self.draw_map()
+        self.draw_score()
+        self.draw_result()
 
     def __export__(self):
         return {
